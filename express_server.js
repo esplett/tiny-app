60baada8644b218c462871d9f41f6998a0f651e8
esplett <estherina@gmail.com>
2018-12-04 21:50:00 +0000

    added all the files i started for tiny app project

---
+++ express_server.js
@@ -0,0 +1,40 @@
+
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs")
+
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+//registers handler on root path
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+//root handler for urls
+app.get("/urls", (req, res) => {
+  res.render("urls_index", { urls: urlDatabase });
+});
+
+//new route
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
